ae1208435be12123e52b23f40a875f2dc6d64549
kinawy <sameh.yasser.kinawy@gmail.com>
2020-07-15 17:22:58 -0400

    yes4

--- app.js
+++ app.js
@@ -6,4 +6,19 @@ let home = 'Boston';
 
 let fav = 5;
 
-let favTeams = ['celts', 'lakers', 'barca']
+let favTeams = ['celts', 'lakers', 'barca', 'liverpool', 'bruins'];
+
+let number = 10;
+let otherNumber = 1;
+while (otherNumber < number) {
+    otherNumber=otherNumber+1;
+    console.log(otherNumber);
+}
+
+let numberTwo = 20;
+let myOtherNumber = 10;
+
+while (myOtherNumber<numberTwo) {
+    myOtherNumber +=1;
+    console.log(myOtherNumber)
+}
