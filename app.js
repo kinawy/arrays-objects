a6441be5f53dd063a853f86a9aad07310a6f6761
kinawy <sameh.yasser.kinawy@gmail.com>
2020-07-15 17:53:30 -0400

    test for loop

--- app.js
+++ app.js
@@ -38,3 +38,13 @@ console.log(samehFave);
 
 
 // For Loops, extra practice
+
+for (let i=0; i<10;i++) {
+    console.log(i); //0,1,2,3,4,5,6,7,8,9
+}
+
+//print teams
+for (let i = 0; i<favTeams.length; i++) {
+    let eachTeam = favTeams[i];
+    console.log(eachTeam);
+}
